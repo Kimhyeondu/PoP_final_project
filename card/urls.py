0f1deb1bb9b9946b340a36bf3a6b5bf41f434837
GrolarKim <audwnswmf@naver.com>
2022-03-14 14:59:25 +0900

    카드 작성 페이지를 장식-메시지 페이지와 선물-완성미리보기 페이지 2가지로 분할 및 디자인 적용

--- card/urls.py
+++ card/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("write/", views.write, name="card_write"),
+    path("write/", views.card_write, name="card_write"),
+    path("gift/", views.card_gift, name="card_gift"),
 ]
--- card/views.py
+++ card/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 # Create your views here.
-def write(request):
+def card_write(request):
+    if request.method == "POST":
+        return redirect("card_gift")
     return render(request, "card/card_write.html")
+
+def card_gift(request):
+    return render(request, "card/card_gift.html")
