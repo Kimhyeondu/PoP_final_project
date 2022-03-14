916ca3270712cba69737ae8d7c8c43d31f1e683d
GrolarKim <audwnswmf@naver.com>
2022-03-14 16:27:37 +0900

    장식 선택 리스트 추가 및 두페이지간 POST연결을  fetch로 바꾸고 대기시간동안 모달창 띄움

--- card/views.py
+++ card/views.py
@@ -1,9 +1,12 @@
+import time
 from django.shortcuts import redirect, render
 
 
 # Create your views here.
 def card_write(request):
     if request.method == "POST":
+        print(request.POST)
+        time.sleep(1)
         return redirect("card_gift")
     return render(request, "card/card_write.html")
 
