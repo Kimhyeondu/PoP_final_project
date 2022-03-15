9bcd69a3a2ba25849f356a34bf30e1f53ab9c427
hyeondu <bbb6045@naver.com>
2022-03-15 19:23:16 +0900

    profile_edit html + js파일 추가 (tag별 클릭이벤트 설정 완료)

--- static/js/edit_profile.js
+++ static/js/edit_profile.js
@@ -1,3 +1,45 @@
+
+function eventHandler(e) {
+    var $eTarget = $(e.currentTarget);
+    // var $targetPanel = $('[aria-labelledby="' + $eTarget.attr('id') + '"]');
+    if($eTarget.hasClass("active")===true){
+        $eTarget
+            .attr('aria-selected', false)
+            .removeClass('active')
+    }
+    else if($eTarget.hasClass("active")===false){
+        $eTarget
+            .attr('aria-selected', true)
+            .addClass('active') // 구버전 IE
+    }
+
+        /*
+        .siblings('[role="option"]')   //형제요소들
+        .attr('aria-selected', false)  //영역선택 해제
+        .removeClass('active'); // 구버전 IE*/
+
+    // $targetPanel
+    //     .attr('aria-hidden', false)
+    //     .addClass('panel') // 구버전 IE
+    //     .siblings('[role="tabpanel"]')
+    //     .attr('aria-hidden', true)
+    //     .removeClass('panel'); // 구버전 IE
+}
+
+function getitem() {
+    var chk_arr = [];
+    $("input[name=tag-list]:checked").each(function () {
+
+        var chk = $(this).val();
+        chk_arr.push(chk);
+        console.log(chk)
+    })
+
+}
+
+$('[role="option"]').on('click', eventHandler);
+//
+$('[role="button"]').on('click', getitem);
 function readURL(input) {
             if (input.files && input.files[0]) {
                 var reader = new FileReader();
