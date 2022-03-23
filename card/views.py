7cf9a212c66986b2feb629dd6bb73e6200a6921b
GrolarKim <audwnswmf@naver.com>
2022-03-23 20:24:35 +0900

    카드 작성 페이지에서 장식 선택 추가 및 css 그리드화 하면서 디테일 살짝 손봄

--- card/views.py
+++ card/views.py
@@ -1,15 +1,16 @@
-from asgiref.sync import async_to_sync
+from asgiref.sync import async_to_sync, sync_to_async
 
 from django.http import Http404, HttpRequest, JsonResponse
 from django.shortcuts import redirect, render
 from card.services.message_service import create_msg
+from card.services.deco_service import all_list_deco
 from user.models import User
 
 
 # Create your views here.
-def card_write(request:HttpRequest, id:int):
+async def card_write(request:HttpRequest, id:int):
     try:
-        User.objects.get(id=id)
+        await sync_to_async(User.objects.get)(id=id)
         if request.method == "POST":
             to_user_id = request.POST["to_user_id"]
             gift_id = request.POST["gift_id"]
@@ -17,10 +18,12 @@ def card_write(request:HttpRequest, id:int):
             deco = request.POST["deco"]
             title = request.POST["title"]
             author = request.POST["author"]
-            async_to_sync(create_msg)(to_user_id=to_user_id, gift_id=gift_id, msg=msg, deco=deco, title=title, author=author)
+            await create_msg(to_user_id=to_user_id, gift_id=gift_id, msg=msg, deco=deco, title=title, author=author)
             
             return JsonResponse({"server":"저장 완료!"})
-        return render(request, "card/card_write.html", {"to_user_id":id})
+        deco_list = await all_list_deco()
+        return await sync_to_async(render)(request, "card/card_write.html", {"to_user_id":id, "deco_list":deco_list})
     except:
+        
         raise Http404("존재하지 않는 유저입니다.")
     
