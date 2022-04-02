530e079d39e9b247ce3bd933c9b914ae89056fb7
LAPTOP-GFCI7DP6\kjyeo <minnjeong07@gmail.com>
2022-04-02 21:12:20 +0900

    메인페이지 html파일이랑 유저별 카드모아서 보여주는 페이지 html 별도로 둠

--- post/views.py
+++ post/views.py
@@ -1,30 +1,26 @@
 from asgiref.sync import sync_to_async
 from django.shortcuts import redirect, render
-from card.models import Message
-from django.contrib.auth.decorators import login_required
 from card.services.message_service import list_to_user_msg
-
-
-# @login_required(login_url='signin')
 from user.models import User
 
-
-def first_page(request):
+def main(request):
     return render(request, "main.html")
 
 
-async def main(request, username:str):
+async def CardList(request, username:str):
     user = await sync_to_async(User.objects.get)(username=username)
     user_id = user.id
     listall = await list_to_user_msg(user_id)
     if listall:
-        return await sync_to_async(render)(request, "main.html", {"listall": listall, "user_id":user_id})
+        return await sync_to_async(render)(request, "card_list.html", {"listall": listall, "user_id":user_id})
     else:
-        return await sync_to_async(render)(request, "main.html", {"user_id":user_id})
-
+        return await sync_to_async(render)(request, "card_list.html", {"user_id":user_id})
 
 
 def upload(request):
     return redirect(request, "post/upload.html")
+
+
+
 
 
