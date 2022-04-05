f0e8907f683448f0c7ea79e7360e5ad88a382e7e
LAPTOP-GFCI7DP6\kjyeo <minnjeong07@gmail.com>
2022-04-05 16:08:54 +0900

    user models.py 필드 추가, 메시지 없을 때 페이지 수정

--- post/views.py
+++ post/views.py
@@ -1,31 +1,36 @@
 from asgiref.sync import sync_to_async
 from django.shortcuts import redirect, render
-
-from card.models import Message
 from card.services.message_service import list_to_user_msg
 from user.models import User
+
 
 def main(request):
     return render(request, "main.html")
 
 
-async def CardList(request, username:str):
-    user = await sync_to_async(User.objects.get)(username=username)
-    user_id = user.id
-    listall = await list_to_user_msg(user_id)
-    if listall:
-        return await sync_to_async(render)(request, "card_list.html", {"listall": listall, "user_id":user_id, "username" : username})
-    else:
-        return await sync_to_async(render)(request, "card_list.html", {"user_id":user_id, "username" : username})
+async def CardList(request, username: str):
+    try:
+        user = await sync_to_async(User.objects.get)(username=username)
+        user_id = user.id
+        listall = await list_to_user_msg(user_id)
+        user_img = user.profile_img
+        bio = user.bio
+        if listall:
+            return await sync_to_async(render)(request, "card_list.html",
+                                               {"listall": listall, "user_id": user_id, "username": username,
+                                                "user_img": user_img, "bio": bio})
+        else:
+            return await sync_to_async(render)(request, "card_list.html",
+                                               {"user_id": user_id, "username": username, "user_img": user_img,
+                                                "bio": bio})
+    except User.DoesNotExist:
+        return redirect('/sign-up/')
 
-async def id_to_username(request, user_id:int):
+
+async def id_to_username(request, user_id: int):
     user = await sync_to_async(User.objects.get)(id=user_id)
     return redirect(f'/{user.username}')
 
+
 def upload(request):
     return redirect(request, "post/upload.html")
-
-
-
-
-
--- user/views.py
+++ user/views.py
@@ -217,6 +217,7 @@ def sign_up_view(request):
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
         password2 = request.POST.get('password2', '')
+        bio = request.POST.get('bio', '')
 
         if password != password2:   # 비밀번호 확인이 틀렸을 때
             return render(request, 'user/signup.html', {'error': '패스워드를 확인해 주세요'})
@@ -228,7 +229,7 @@ def sign_up_view(request):
             if exist_user:  # 아이디 중복 확인
                 return render(request, 'user/signup.html', {'error': '사용중인 이름 입니다.'})
             else:   # 유저 생성
-                User.objects.create_user(username=username, password=password)
+                User.objects.create_user(username=username, password=password,  bio=bio)
                 return redirect('/sign-in')
 
 
@@ -255,4 +256,4 @@ def sign_in_view(request):
 @login_required
 def logout(request):
     auth.logout(request)
-    return redirect('/')
+    return redirect('/sign-in/')
